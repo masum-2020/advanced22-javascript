09189e3bb17c1e0c43d5fd2f43ad8ab26e2cfafe
masum-2020 <almsm014@gmail.com>
2022-02-13 11:24:29 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,60 @@
+// Falsy
+// 0
+// ""
+// undefined
+// null
+// Truthy
+// '0', ' ', [], {}
+
+// const age = 4;
+
+// if(age > 0){
+//    console.log("condition is true"); 
+// }
+// else{
+//     console.log("condition is false");
+// }
+
+// const name = "Saifullah";
+// if(name.length > 0){
+//     console.log("condition is true"); 
+//  }
+//  else{
+//      console.log("condition is false");
+//  }
+
+//  let name;
+//  console.log(name)
+//  if(name){
+//     console.log("condition is true"); 
+//  }
+//  else{
+//      console.log("condition is false");
+//  }
+
+
+// let name = null;
+//  if(name){
+//     console.log("condition is true"); 
+//  }
+//  else{
+//      console.log("condition is false");
+//  }
+
+
+// let name = 0;
+// if(name || name == 0){
+//     console.log("condition is true"); 
+//  }
+//  else{
+//      console.log("condition is false");
+//  }
+
+ 
+let name = 12;
+if(name || name == 0){
+    console.log("condition is true"); 
+ }
+ else{
+     console.log("condition is false");
+ }
