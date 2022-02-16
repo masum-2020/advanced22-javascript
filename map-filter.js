f6fd091876962c3b012915b0c4e7d7fb971b589e
masum-2020 <almsm014@gmail.com>
2022-02-16 08:32:41 +0600

    Onek  kisu Shiklam

---
+++ map-filter.js
@@ -0,0 +1,17 @@
+const numbers = [2, 3, 4, 5, 6];
+// const output = [];
+
+// for(let i = 0; i < numbers.length; i++){
+//     const element = numbers[i]
+//     const result = element * element;
+//     output.push(result);
+// }
+
+
+function square (element){
+    return elemen * element;
+}
+numbers.map(function(element){
+    console.log(element);
+})
+// console.log(output);
